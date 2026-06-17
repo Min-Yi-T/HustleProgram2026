286f1e6b1536b220b80dc7c2834ba1610d92b9c4
Yuu <yu@Yuus-MacBook-Pro.local>
2026-06-17 14:12:36 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,20 @@
+
+first_name = "Min Yi"
+last_name = "Truong"
+nickname = "Min Yi or Min"
+age = 14
+used_python_before = False
+hobbies = ["drawing","sleeping","reading"]
+favorites = {"Movies": "Emoji's movie?", "Food": "None", "Hobby": "Drawing"}
+skills = ["Honestly","Hardworking"]
+languages_spoken = {"English","Cantonese"}
+
+print(first_name)
+print(last_name)
+print(nickname)
+print(age)
+print(used_python_before)
+print(hobbies)
+print(favorites)
+print(skills)
+print(languages_spoken)
